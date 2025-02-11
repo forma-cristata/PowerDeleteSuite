1697afa35e31a6ce391a1efefdb7b60c69ff4a40
kcraycraft45 <kcraycraft45@southhills.edu>
2025-02-11 18:19:21 -0500

    Added licensing header to all code files as well as a licensing blurb to README.md

--- powerdeletesuite.js
+++ powerdeletesuite.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of PowerDeleteSuite.
+ *
+ * PowerDeleteSuite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PowerDeleteSuite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 2
+ * along with PowerDeleteSuite.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var pd = {
   version: "1.4.11",
   bookmarkver: "1.4",
